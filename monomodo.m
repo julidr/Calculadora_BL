0139fb52de86270d5ab1f659c4072cfadf776abe
Juliana Diaz <juli.milena@hotmail.com>
2017-05-13 13:32:24 -0500

    Arreglo de GUI y Algoritmo para Monomodos

--- monomodo.m
+++ monomodo.m
@@ -1,8 +1,31 @@
-function calcularBL = monomodo(dispersion, lambda)
-try
+function calcularBL = monomodo(dispersion, lambda, B, L)
+clc;
+
+if dispersion == 0 || lambda == 0
+    calcularBL = 'No se puede calcular el BL';
+    return
+else 
     BL = 1/(dispersion*lambda);
-    calcularBL = BL;
-catch
-    warning('Por favor solo ingresar valores numericos')
-    calcularBL = 'null';
+end
+
+if B == 0
+    if L == 0
+        B = NaN;
+        L = NaN;
+        calcularBL = [BL, B, L];
+    else
+        B = 1/(dispersion*lambda*L);
+        BL = B*L;
+        calcularBL = [BL, B, L];
+    end
+else
+    if B == 0
+        B = NaN;
+        L = NaN;
+        calcularBL = [BL, B, L];
+    else
+        L = 1/(dispersion*lambda*B);
+        BL = B*L;
+        calcularBL = [BL, B, L];
+    end
 end
